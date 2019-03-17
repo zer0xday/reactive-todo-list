b29f23b9916b25c42229d34127294385a80319b4
zer0xday <majcher10@gmail.com>
2019-03-17 22:44:57 +0100

    update

--- index.php
+++ index.php
@@ -5,6 +5,21 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require 'vendor/autoload.php';
 
 $app = new Slim\App;
+$container = $app->getContainer();
+
+$container['view'] = function($container) {
+    $view = new \Slim\Views\Twig('views');
+
+     $router = $container->get('router');
+     $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+     $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
+
+     return $view;
+};
+
+$app->get('/', function(Request $req, Response $res) {
+    return $this->view->render($res, 'index.twig');
+});
 
 $app->get('/{name}', function(Request $req, Response $res, $args) {
     $name = $args['name'];
