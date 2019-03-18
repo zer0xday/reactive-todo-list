04d8bf19db21f04d6e61bff570d81c511569525b
zer0xday <majcher10@gmail.com>
2019-03-18 06:20:23 +0100

    app update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # college_app
 To run this app you must have PHP installed on your machine then execute CMD command in app directory:
-> php -S localhost:8080 <You can change the "8080" part to another port if this one is taken>
+> php -S localhost:8080 <You can change the "8080" part to another port if this one is taken. (max port range: 9999)>
 
 Then you can paste or type in your browser this:
 > localhost:8080
--- index.php
+++ index.php
@@ -3,22 +3,30 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
+require 'config.php';
 
-$app = new Slim\App;
+$app = new Slim\App(['settings' => $config]);
 $container = $app->getContainer();
 
+// set twig to view renderer engine
 $container['view'] = function($container) {
     $view = new \Slim\Views\Twig('views');
 
-     $router = $container->get('router');
-     $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
-     $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
 
-     return $view;
+    return $view;
 };
 
 $app->get('/', function(Request $req, Response $res) {
-    return $this->view->render($res, 'index.twig');
+    return $this->view->render($res, 'pages/home.html');
+});
+
+//-------------- Login ------------------
+$app->post('/login', function (Request $request, Response $response, $args) use ($app) {
+    $json =$request->getParams();
+    return $response->withJson($json);
 });
 
 $app->get('/{name}', function(Request $req, Response $res, $args) {
